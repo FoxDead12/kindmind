047112ba434469d92aecf893c619548bffb40524
David Xavier <davidxavier7512@gmail.com>
2023-12-02 12:34:10 +0000

    add migration system

---
+++ migration.js
@@ -0,0 +1,16 @@
+import mysql from 'mysql';
+import migration from 'mysql-migrations'
+import path from 'path'
+
+var connection = mysql.createPool({
+  connectionLimit : 10,
+  host     : 'localhost',
+  user     : 'kindmind',
+  password : 'kindmind',
+  database : 'kindmind',
+  insecureAuth : true
+});
+
+migration.init(connection, path.join('./db/migrations'), function() {
+  console.log("finished running migrations");
+});
