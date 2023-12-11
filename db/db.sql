1afd47d688f4d036a39d83e18c286989dea53878
David Xavier <davidxavier7512@gmail.com>
2023-12-11 00:04:52 +0000

    db complete (i guess)

--- db/db.sql
+++ db/db.sql
@@ -1,51 +1,88 @@
-DROP TABLE users;
-DROP TABLE teacher_info;
 
 
 CREATE TABLE subjects (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(50) NOT NULL,
-  create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (id)
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE location (
-  id INT NOT NULL AUTO_INCREMENT,
-  country VARCHAR(50) NOT NULL,
-  city VARCHAR(50) NOT NULL,
-  PRIMARY KEY (id)
+CREATE TABLE education_level (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE locations (
+    id INT NOT NULL AUTO_INCREMENT,
+    country VARCHAR(50) NOT NULL,
+    city VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE users (
-  id INT NOT NULL AUTO_INCREMENT,
-  email VARCHAR(255) NOT NULL,
-  full_name VARCHAR(255) NOT NULL,
-  hash TEXT NOT NULL,
-  role INT NOT NULL,
-  activate TINYINT(1) DEFAULT 0,
-  image_url TEXT DEFAULT NULL,
-  location_id VARCHAR(50) DEFAULT NULL,
-  create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (id),
-  FOREIGN KEY (location_id) REFERENCES location(id)
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL,
+    full_name VARCHAR(255) NOT NULL,
+    hash TEXT NOT NULL,
+    role INT NOT NULL,
+    activate TINYINT(1) DEFAULT 0,
+    image_url TEXT DEFAULT NULL,
+    location_id INT DEFAULT NULL,
+    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    FOREIGN KEY (location_id) REFERENCES locations(id)
+);
+
+CREATE TABLE teacher_information (
+    id INT NOT NULL AUTO_INCREMENT,
+    id_user INT NOT NULL,
+    hour_payment FLOAT NOT NULL,
+    description TEXT NOT NULL,
+    about_class TEXT NOT NULL,
+    online TINYINT(1) NOT NULL,
+    presencial TINYINT(1) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_user) REFERENCES users(id)
 );
 
 CREATE TABLE teacher_subjects (
-  id INT NOT NULL AUTO_INCREMENT,
-  id_user INT NOT NULL,
-  id_subject INT NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (id_user) REFERENCES users(id),
-  FOREIGN KEY (id_skill) REFERENCES skills(id)
+    id INT NOT NULL AUTO_INCREMENT,
+    id_user INT NOT NULL,
+    id_subject INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_user) REFERENCES users(id),
+    FOREIGN KEY (id_subject) REFERENCES subjects(id)
 );
 
-CREATE TABLE teacher_info (
-  id INT NOT NULL AUTO_INCREMENT,
-  id_user INT NOT NULL,
-  hour_payment FLOAT NOT NULL,
-  description VARCHAR(255) DEFAULT NULL,
-  tsv tsvector DEFAULT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (id_user) REFERENCES users(id)
+CREATE TABLE teacher_education_level (
+    id INT NOT NULL AUTO_INCREMENT,
+    id_user INT NOT NULL,
+    id_level INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_user) REFERENCES users(id),
+    FOREIGN KEY (id_level) REFERENCES education_level(id)
+);
+
+CREATE TABLE classes (
+    id INT NOT NULL AUTO_INCREMENT,
+    id_student INT NOT NULL,
+    id_professor INT NOT NULL,
+    start_time TIMESTAMP NOT NULL,
+    duration FLOAT NOT NULL,
+    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_student) REFERENCES users(id),
+    FOREIGN KEY (id_professor) REFERENCES users(id)
+);
+
+CREATE TABLE messages (
+    id INT NOT NULL AUTO_INCREMENT,
+    send_user INT NOT NULL,
+    receiver_user INT NOT NULL,
+    message TEXT NOT NULL,
+    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    FOREIGN KEY (send_user) REFERENCES users(id),
+    FOREIGN KEY (receiver_user) REFERENCES users(id)
 );
