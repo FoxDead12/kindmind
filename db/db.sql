3aef0f1cb51639dd3eedbe26d4961d5aedb9faf3
NoxPT <132589530+NoxPT@users.noreply.github.com>
2023-12-20 17:01:47 +0000

    Função filtro de paises teste

--- db/db.sql
+++ db/db.sql
@@ -1,7 +1,7 @@
 CREATE TABLE subjects (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
     FULLTEXT (name)
 );
 
--- http-server/data/location.php
+++ http-server/data/location.php
@@ -3,15 +3,16 @@
     
 	class Location extends Request {
 		public function execute () {
-            //$cleanString = preg_replace('/[^A-Za-z0-9-]/', '', $inputString);
-
             $field = $this->body['field'];
             $field = preg_replace('/[^A-Za-z0-9-]/', '', $field);
             if ($field == null || $field == '') {
                 return $this->send_message('Invalid data!!', 400);
               }
-           // 'SELECT * FROM locations WHERE country LIKE %vsti%'
-
+            $field = '%'.$field.'%';
+            $result = $this->db->execute_query('SELECT * FROM locations WHERE country LIKE (?)',[$field]);
+            $body = ['result' => $result->fetch_all()];  
+            
+            $this->send_message('', 200, $body)
 	}
 }
 
